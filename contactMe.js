5ae86d90a427c9b6659986019292a87bb8dce308
Adian Hooper <97369087+SmallGraphics@users.noreply.github.com>
2024-07-01 14:16:53 -0500

    Added background image and finalized contact me page

--- contactMe.js
+++ contactMe.js
@@ -1,5 +1,7 @@
 function clipboard() {
     navigator.clipboard.writeText("adianhooper@gmail.com");
-    document.querySelector("#GmailText").innerHTML = "adianhooper@gmail.com<br><br><br>copied to clipboard";
-    document.querySelector("#GmailText").style.marginTop = "-2.1%";
+    document.querySelector("#gmailLabel").innerHTML = "copied to clipboard";
+    document.querySelector("#GmailText").style.marginTop = "0%";
+    document.querySelector("#Github").style.marginTop = "-50%";
+
 };
